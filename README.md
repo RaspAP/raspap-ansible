1f2bec611ad2814244307f50f82177614ab65ed5
Federico Frigo <ffrigo8@gmail.com>
2021-04-22 12:13:07 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # raspap-ansible
+
 Ansible playbook for provisioning RaspAP
+
+## Prerequisites
+
+### Ansible
+
+```bash
+# install ansible
+sudo apt update
+sudo apt install software-properties-common python3-pip
+pip3 install ansible
+
+# [OPTIONAL] install sshpass if login to raspberry pi is made with password instead of ssh key
+sudo apt install sshpass
+```
+
+### Inventory
+
+In `inventory` folder:
+
+- `hosts`: default configuration will handle only one Raspberry Pi, so optionally add Raspberry Pi(s) that will be configured, with incremental numbers (e.g. raspberrypi01, raspberrypi02, ...)
+- `host_vars`: for all hosts in `hosts` create a `raspberrypiXX.yaml` with inside its `ansible_host: <ip_address>`
+- `group_vars`: choose between `ansible_ssh_pass` or `ansible_ssh_private_key_file` and give it the correct value, then configure the other parameters to match the desired configuration
+
+## Configure RaspAP
+
+To configure RaspAP in the chosen Raspberry Pi(s):
+
+```bash
+ansible-playbook site.yaml
+```
+
+To rollback what `site.yaml` did:
+
+```bash
+ansible-playbook site-rollback.yaml
+```
---
+++ roles/lighttp/templates/config.php
@@ -0,0 +1,49 @@
+<?php
+
+define('RASPI_BRAND_TEXT', "'{{ raspi_brand_text }}'");
+define('RASPI_CONFIG', '/etc/raspap');
+define('RASPI_CONFIG_NETWORK', RASPI_CONFIG.'/networking/defaults.json');
+define('RASPI_ADMIN_DETAILS', RASPI_CONFIG.'/raspap.auth');
+define('RASPI_WIFI_AP_INTERFACE', 'wlan0');
+define('RASPI_CACHE_PATH', sys_get_temp_dir() . '/raspap');
+
+// Constants for configuration file paths.
+// These are typical for default RPi installs. Modify if needed.
+define('RASPI_DNSMASQ_LEASES', '/var/lib/misc/dnsmasq.leases');
+define('RASPI_DNSMASQ_PREFIX', '/etc/dnsmasq.d/090_');
+define('RASPI_ADBLOCK_LISTPATH', '/etc/raspap/adblock/');
+define('RASPI_ADBLOCK_CONFIG', RASPI_DNSMASQ_PREFIX.'adblock.conf');
+define('RASPI_HOSTAPD_CONFIG', '/etc/hostapd/hostapd.conf');
+define('RASPI_DHCPCD_CONFIG', '/etc/dhcpcd.conf');
+define('RASPI_WPA_SUPPLICANT_CONFIG', '/etc/wpa_supplicant/wpa_supplicant.conf');
+define('RASPI_HOSTAPD_CTRL_INTERFACE', '/var/run/hostapd');
+define('RASPI_WPA_CTRL_INTERFACE', '/var/run/wpa_supplicant');
+define('RASPI_OPENVPN_CLIENT_CONFIG', '/etc/openvpn/client/client.conf');
+define('RASPI_OPENVPN_CLIENT_LOGIN', '/etc/openvpn/client/login.conf');
+define('RASPI_OPENVPN_SERVER_CONFIG', '/etc/openvpn/server/server.conf');
+define('RASPI_TORPROXY_CONFIG', '/etc/tor/torrc');
+define('RASPI_LIGHTTPD_CONFIG', '/etc/lighttpd/lighttpd.conf');
+define('RASPI_ACCESS_CHECK_IP', '1.1.1.1');
+define('RASPI_ACCESS_CHECK_DNS', 'one.one.one.one');
+
+// Constant for the 5GHz wireless regulatory domain
+define('RASPI_5GHZ_ISO_ALPHA2', array('NL','US'));
+define('RASPI_5GHZ_MAX_CHANNEL', 165);
+
+// Optional services, set to true to enable.
+define('RASPI_WIFICLIENT_ENABLED', true);
+define('RASPI_HOTSPOT_ENABLED', true);
+define('RASPI_NETWORK_ENABLED', true);
+define('RASPI_DHCP_ENABLED', true);
+define('RASPI_ADBLOCK_ENABLED', false);
+define('RASPI_OPENVPN_ENABLED', "{{ openvpn_enabled }}");
+define('RASPI_TORPROXY_ENABLED', false);
+define('RASPI_CONFAUTH_ENABLED', true);
+define('RASPI_CHANGETHEME_ENABLED', true);
+define('RASPI_VNSTAT_ENABLED', true);
+define('RASPI_SYSTEM_ENABLED', true);
+define('RASPI_MONITOR_ENABLED', "{{ monitor_enabled }}");
+
+// Locale settings
+define('LOCALE_ROOT', 'locale');
+define('LOCALE_DOMAIN', 'messages');
